2d4aef6c37355d0c3afbad31b263bf575d0abd7a
Priyansh Saxena <priyanshsaxena43@gmail.com>
2026-05-04 13:05:22 +0530

    deployment setup

--- app.py
+++ app.py
@@ -47,4 +47,6 @@ def recommend():
 
 # 🚀 Run server
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
